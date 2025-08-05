2f0370f1806c5b455479e4a476a62b3b6ee40e1f
najwanovalina <novallinenajzzr@gmail.com>
2025-08-05 15:08:57 +0700

    Add files via upload

---
+++ temannj.java
@@ -0,0 +1,74 @@
+public class temannj {
+    public static void main(String[] args) {
+        
+        String namaLengkap = "Ribhki Fadhilatunn Naura";
+        String asalSekolah = "SMPN 2 Tuban";
+        int umur = 16;
+        char jk ='P';
+
+        System.out.println (namaLengkap);
+        System.out.println(asalSekolah);
+        System.out.println (umur + " tahun ");
+        System.out.println("Jenis Kelamin : " + jk);
+        System.out.println();
+    
+        String namaLengkap1 = "Angggun Ayyuning Putri";
+        String asalSekolah1 = "MTSN 2 Malang";
+        int umur1 = 15;
+        char jk1 ='P';
+
+        System.out.println (namaLengkap1);
+        System.out.println(asalSekolah1);
+        System.out.println (umur1);
+        System.out.println("Jenis Kelamin : " + jk);
+        System.out.println();
+    
+        String namaLengkap2 = "Savgi Caresse Rizki Azaria";
+        String asalSekolah2 = "MTSN 2 Malang";
+        int umur2 = 15;
+        char  jk2 = 'P';
+    
+        System.out.println (namaLengkap2);
+        System.out.println(asalSekolah2);
+        System.out.println (umur2);
+        System.out.println("Jenis Kelamin : " + jk);
+        System.out.println();
+    
+        
+        String namaLengkap3 = "Aurelya Ramadhani";
+        String asalSekolah3 ="SMPN 1 JABUNG";
+        int umur3 = 14;
+        char jk3 ='P';
+        
+        System.out.println (namaLengkap3);
+        System.out.println(asalSekolah3);
+        System.out.println (umur3);
+        System.out.println("Jenis Kelamin : " + jk);
+        System.out.println();
+    
+
+        String namaLengkap4 = "Jasmine Nazwa Medina";
+        String asalSekolah4 ="SMPN 1 Lawang";
+        int umur4 = 15;
+        char jk4='P';
+
+        System.out.println (namaLengkap4);
+        System.out.println(asalSekolah4);
+        System.out.println (umur4);
+        System.out.println("Jenis Kelamin : " + jk);
+        System.out.println();
+    
+        String namaLengkap5= "Kinanti Elok Hati Nurani";
+        String asalSekolah5 ="SMPN 1 Pati";
+        int umur5 = 14;
+        char jk5='P';
+
+        System.out.println (namaLengkap5);
+        System.out.println(asalSekolah5);
+        System.out.println (umur5);
+        System.out.println("Jenis Kelamin : " + jk);
+        System.out.println();
+    
+
+    }
+}
